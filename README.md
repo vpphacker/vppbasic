e94b5881dc6453e680774e103bed6d4ddbfa2559
VPPHacker <48642846+vpphacker@users.noreply.github.com>
2019-04-22 19:28:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# vppbasic
+﻿If you are looking for basic packages of termux then this is right tool for you. Follow the given commands and install basic packages in termux. And This Tool is made for Termux beginner.And it's a very easy to use and install. This tool is coded in python . 
+
+• Tool Installs This packages
+ 
+   python
+   python2
+   python2-dev
+   python3
+   git
+   php 
+   perl 
+   bash
+   nano
+   curl
+   openssl
+   openssh
+   wget
+   clang
+   help
+   nmap
+   w3m
+   hydra
+   ruby
+   macchanger
+   dnsutils
+   coreutils
+   host
+
+
+This command for access the storage in termux 
+termux-setup-storage.
+
+
+• Installation 
+ 
+ Termux
+   
+   $ apt install git 
+   $ apt install python 
+   $ git clone https://githubcom/vpphacker/vppbasic
+   $ cd vppbasic 
+   $ ls
+   $ chmod +x *
+   $ ls
+   $ python vppbasic.py
+
+This will take too much time it's depends on your data connection. 
+
+Visit on YouTube Channel
+
+https://www.youtube.com/channel/UCIQ5wO7zmPmjnIBm1d_fGKw
+
---
+++ vppbasic.py
@@ -0,0 +1,67 @@
+﻿import os
+
+print ("vpp hacker")
+print ("coded by vpp hacker")
+
+
+os.system ("apt update")
+os.system ("atp upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+
+print ("wait for second and start hacking")
+
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+
+
+print ("""
+subscribe VPP Hacker """)
+
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+
+print ("Allow the Button For Access the Storage in Termux")
+
+
+os.system ("termux-setup-storage")
+
+
+print ("""
+
+
+               )
+              (
+        /\  .-:;;-.  /\
+       //\\/  ,,,  \//\\
+       |/\| ,;;;;;, |/\|
+       //\\\;-...-;///\\
+      //  \/   .   \/  \\
+     (| ,-_| \ | / |_-, |)
+       //`__\.-.-./__`\\
+      // /.-(() ())-.\ \\
+     (\ |)   '---'   (| /)
+      ` (| Coded By   |) `
+        \) VPP Hacker (/
+
+visit us on YouTube Channel For Hacking Videos
+This is Basic Tool of packge installing and this is part 2 of basic termux command
+""")
+print ("this will install 24 basic packges")
